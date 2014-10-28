95f699d8cf4b12fa09af979da5ff68e7b88e3e81
Mishal Islam <mishal.islam01@gmail.com>
2014-10-28 16:12:39 +0000

    boris bikes- bike class

---
+++ spec/bike_spec.rb
@@ -0,0 +1,27 @@
+require 'bike'
+
+describe Bike do
+
+	let(:broken_bike) {Bike.new}
+	before(:each) do
+		broken_bike.break!
+	end
+
+	it 'is not broken when created' do
+		new_bike= Bike.new
+		expect(new_bike).not_to be_broken
+		expect(new_bike.broken?).to be false
+	end
+
+	it 'can break' do
+		expect(broken_bike).to be_broken
+	end
+
+	it 'can be fixed' do
+		broken_bike.fix!
+		expect(broken_bike).not_to be_broken
+	end
+end
+
+#can break
+#can be fixed
