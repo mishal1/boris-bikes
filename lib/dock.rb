214bb72f80b86e676fc2c4a936b355b98042ee93
Mishal Islam <mishal.islam01@gmail.com>
2014-10-29 17:22:57 +0000

    refactored dock.rb

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,28 +1,11 @@
+require_relative 'bike_container'
+
 class DockingStation
-	def initialize(capacity)
-		@capacity = capacity
-		@bike =[]
-#without options
+	include BikeContainer
+DEFAULT_CAPACITY =10
+	def initialize(options= {})
+		self.capacity = options.fetch(:capacity, capacity)
 	end	
 
-	def bike_count
-		@bike.count
-	end
-
-	def dock(bike)
-		raise "Station is full" if full?
-		@bike << bike
-	end
-
-	def release(bike)
-		@bike.delete(bike)
-	end
-
-	def full?
-		bike_count == @capacity
-	end
-
-	def available_bikes
-		@bike.reject {|bike| bike.broken?}
-	end
+	
 end
--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -3,10 +3,10 @@ require "dock"
 describe DockingStation do 
 
 	let(:bike) {Bike.new}
-	let(:station) {DockingStation.new(20)}
+	let(:station) {DockingStation.new(:capacity => 20)}
 
 	def fill_station
-		20.times{station.dock(bike)}
+		20.times {station.dock(Bike.new)}
 	end
 
 	it "should be able to accept bikes" do
@@ -23,16 +23,16 @@ describe DockingStation do
 
 	it "should know when the docking station is full" do
 		expect(station).not_to be_full
-		fill_station
+		20.times {station.dock(Bike.new)}
 		expect(station).to be_full
 	end
 
 	it "shouldn't accept a bike when full" do
-		fill_station
+		20.times {station.dock(Bike.new)}
 		expect(station.bike_count).to eq(20)
 		expect(lambda {station.dock(bike)}).to raise_error(RuntimeError)
-
 	end
+	#put {} when raising errors rather than (). Have to use code that is going to break with a block
 
 	it "should provide a list of available bikes" do
 		working, broken = Bike.new, Bike.new
@@ -45,8 +45,16 @@ describe DockingStation do
 end
 
 
+
+
+
+
 #Must be able to accept bikes
 #Must be able to release bikes
 #Must have capacity limit
 #If a bike is broken, the docking station shouldn't release
+
+#working = double : bike, broken?:false
+#double- take a method and then hashes(key & value)
+#allow(working).to recieve(:broken?).and_return(false)
 
